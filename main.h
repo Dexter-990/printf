d65cdf319a20d229d65992f1c7bf2ead8373cb40
Dexter <dexter.dev17@gmail.com>
2023-06-28 08:21:25 +0000

    Done

--- main.h
+++ main.h
@@ -12,10 +12,16 @@ int print_type(char spec, va_list ap, int *x);
 int _strlen(char *s);
 int print_int(va_list ap);
 
-
+/**
+ * struct pas - Short description
+ * @spec: First member
+ * @func: Second member
+ *
+ * Description: Longer description
+ */
 typedef struct pas
 {
 	char spec;
-	int (*func) (va_list ap);
+	int (*func)(va_list ap);
 } def;
 #endif
